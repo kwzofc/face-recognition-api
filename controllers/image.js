dbce1e80761614318e387f401bbdd387d511f18a
kwzofc <kwzofc@live.com>
2021-08-04 19:15:33 +0700

    Fixed bug

--- controllers/image.js
+++ controllers/image.js
@@ -4,7 +4,7 @@ const app = new Clarifai.App({
   apiKey: ''
 });
 
-const handleAPICall = (req, res) => {
+const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
@@ -26,5 +26,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
-  handleAPICall
+  handleApiCall
 };
